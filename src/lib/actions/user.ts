87836e92d7dd948e3fc2d4e590754d6a4bc55fef
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-01-05 16:57:47 +0300

    Refactor user model and enhance MongoDB integration
    - Improved the user model in user.model.ts by refining field definitions and validation for better data integrity.
    - Enhanced MongoDB connection utility in mongoose.ts to ensure robust error handling and prevent multiple connections.
    - Updated TypeScript configuration in tsconfig.json for better type support with Mongoose.
    
    This commit strengthens the user model and database connection, improving overall application performance and reliability.

---
+++ src/lib/actions/user.ts
@@ -0,0 +1,50 @@
+import User from "../models/user.model";
+
+import { connect } from "../mongodb/mongoose";
+
+type clerkInfo = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  image_url: string;
+  email_addresses: { email_address: string }[]; // Corrected type
+  username: string;
+};
+export const createOrUpdateUser = async ({
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email_addresses,
+  username,
+}: clerkInfo) => {
+  try {
+    await connect();
+    const user = await User.findOneAndUpdate(
+      { clerkID: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          profilePicture: image_url,
+          email: email_addresses[0].email_address,
+          username,
+        },
+      },
+      { new: true, upsert: true }
+    );
+
+    return user;
+  } catch (error) {
+    console.log("Error Creating or Updating the user:", error);
+  }
+};
+
+export const deleteUser = async (id: string) => {
+  try {
+    await connect();
+    await User.findOneAndDelete({ clerkID: id });
+  } catch (error) {
+    console.log("Error Deleting the User:", error);
+  }
+};
