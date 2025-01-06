de6d353c529375755ce9772382092eeb7ef3fc6b
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-01-06 09:29:35 +0300

    demo

--- src/lib/actions/user.ts
+++ src/lib/actions/user.ts
@@ -3,25 +3,25 @@ import User from "../models/user.model";
 import { connect } from "../mongodb/mongoose";
 
 type clerkInfo = {
-  id: string | null;
-  first_name: string | null;
-  last_name: string | null;
-  image_url: string | null;
+  id: string;
+  first_name: string;
+  last_name: string;
+  image_url: string;
   email_addresses: { email_address: string }[]; // Corrected type
   username: string;
 };
-export const createOrUpdateUser = async ({
+export const createOrUpdateUser = async (
   id,
   first_name,
   last_name,
   image_url,
   email_addresses,
-  username,
-}: clerkInfo) => {
+  username
+) => {
   try {
     await connect();
     const user = await User.findOneAndUpdate(
-      { clerkID: id },
+      { clerkId: id },
       {
         $set: {
           firstName: first_name,
@@ -33,18 +33,17 @@ export const createOrUpdateUser = async ({
       },
       { new: true, upsert: true }
     );
-
     return user;
   } catch (error) {
-    console.log("Error Creating or Updating the user:", error);
+    console.log("Error creating or updating user:", error);
   }
 };
 
-export const deleteUser = async (id: string) => {
+export const deleteUser = async (id) => {
   try {
     await connect();
-    await User.findOneAndDelete({ clerkID: id });
+    await User.findOneAndDelete({ clerkId: id });
   } catch (error) {
-    console.log("Error Deleting the User:", error);
+    console.log("Error deleting user:", error);
   }
 };
